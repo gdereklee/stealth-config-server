5bb564145517cf3b0eca7a31bfb69fa693c66636
Derek <skdereklee@gmail.com>
2021-08-29 13:55:43 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# stealth-config-server
---
+++ src/test/java/com/stealth/oms/configuration/stealthconfigserver/StealthConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stealth.oms.configuration.stealthconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StealthConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
